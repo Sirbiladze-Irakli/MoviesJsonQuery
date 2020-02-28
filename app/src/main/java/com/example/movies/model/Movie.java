510e931211f1c00aa470ed0e8afc4a18ccac4a94
Irakli <9670451404@mail.ru>
2020-02-28 21:46:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/movies/model/Movie.java
@@ -0,0 +1,34 @@
+package com.example.movies.model;
+
+public class Movie  {
+    private String title;
+    private String url;
+    private String year;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
